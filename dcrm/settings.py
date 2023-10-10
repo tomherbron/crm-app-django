c2042dc5113eb58ffc732115b2e6e4d1a5a15b3d
Tom <tom.herbron@proton.me>
2023-10-10 12:31:56 +0200

    Created views for login/logout, templates, fix template issue in settings

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'dcrm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['website/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
